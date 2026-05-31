cd862575aa94d2e2ddc76c18b01440ddc5ab3d24
Arnav <a.purohit1903@gmail.com>
2026-05-31 19:15:04 +0530

    Added config py to add Hugging Face token

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -8,6 +8,7 @@ class Settings(BaseSettings):
     REDIS_URL: str
     QDRANT_URL: str
     GROQ_API_KEY: str
+    HF_TOKEN: str
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
